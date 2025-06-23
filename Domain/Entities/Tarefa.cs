c1757856352555d445d1b1bf8094b4c3d3e847f9
Rafael Caetano <rafaelkaetanodf@gmail.com>
2025-06-23 20:42:03 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Entities/Tarefa.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Enum;
+
+namespace Domain.Entities;
+
+public class Tarefa
+{
+    public Guid TarefaId { get; set; }
+    
+    public string? Titulo { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public StatusTarefa StatusTarefa { get; set; }
+
+    public DateTime DataVencimento { get; set; }
+
+    public Tarefa()
+    { }
+
+    public Tarefa(Guid tarefaId, string titulo, string descricao, StatusTarefa status, DateTime dataVencimento)
+    {
+        TarefaId = tarefaId;
+        Titulo = titulo;
+        Descricao = descricao;
+        StatusTarefa = status;
+        DataVencimento = dataVencimento;
+    }
+
+    public static Tarefa Criar(string titulo, string descricao, StatusTarefa status, DateTime dataVencimento)
+    {
+        if (string.IsNullOrEmpty(titulo)) 
+            throw new ArgumentNullException(nameof(titulo)); 
+
+        if(string.IsNullOrEmpty(descricao))
+            throw new ArgumentNullException(nameof(descricao));
+
+        if(status != StatusTarefa.EmAndamento 
+            || status != StatusTarefa.Concluido
+            || status != StatusTarefa.Pendente) 
+            throw new ArgumentNullException(nameof(status));
+
+       if(dataVencimento == DateTime.MinValue)
+        {
+            throw new ArgumentNullException(nameof(dataVencimento));
+        }
+        return new Tarefa(Guid.Empty, titulo, descricao, status, dataVencimento);
+    }
+}
---
+++ Domain/Repository/ITarefa.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repository;
+
+public interface ITarefa
+{
+    Task<Tarefa> CreateAsync(Tarefa tarefa);
+    Task DeleteAsync(Tarefa tarefa);
+    Task<Tarefa> UpdateAsync(Tarefa tarefa);
+    Task<Tarefa> GetTarefaByIdAsync(Guid tarefaId);
+    Task<IEnumerable<Tarefa>> GetTarefasAsync();
+}
